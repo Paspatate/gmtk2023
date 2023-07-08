3cf7a84c686938ff5c7c2186f95b5b07f10a4a24
PasPatate <romigami@orange.fr>
2023-07-08 22:50:31 +0200

    temporisation des actions du joueur

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -4,13 +4,18 @@ class_name Player
 signal death
 signal jump
 
+enum TrigType {
+	DIRECTION,
+	JUMP
+}
+
 @export var max_speed = 270
 @export var acceleration : float = 70
 @export var jump_strength : float = 630
 @export var gravity = 2500;
 
 var direction = 1
-
+var action_queue: Array[TrigType] = []
 
 func _ready():
 	$PlayerAnimation.play("walk")
@@ -21,12 +26,22 @@ func _physics_process(delta):
 	velocity.x = direction_vec.x * max_speed
 	velocity.y += gravity * delta
 	
+	for action_index in range(action_queue.size()):
+		if action_queue[action_index] == TrigType.JUMP and is_on_floor():
+			do_jump()
+			action_queue.remove_at(action_index)
+		elif action_queue[action_index] == TrigType.DIRECTION:
+			reverse_direction()
+			action_queue.remove_at(action_index)
+		
+		
+	
 	move_and_slide()
 	
 func do_jump():
 	if is_on_floor():
 		velocity.y = -jump_strength
-	
+
 
 func choose_direction() -> Vector2:
 	return Vector2.RIGHT * direction
@@ -44,9 +59,9 @@ func _on_jump():
 
 func _on_movement_trigger_area_entered(area):
 	if area is JumpTrigger:
-		do_jump()
-		print("player jumped")
+		action_queue.append(TrigType.JUMP)
+		print("added player jump to queue")
 	elif area is DirectionTrigger:
-		reverse_direction()
-		print("reverse the direction")
+		action_queue.append(TrigType.DIRECTION)
+		print("added direction revese to queue")
 		
